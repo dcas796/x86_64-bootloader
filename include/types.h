426956359ede581918ee41e3b292439d98ed4330
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-02-21 16:10:34 +0100

    Add 32-bit types, size_t and PACKED macro

--- include/types.h
+++ include/types.h
@@ -3,9 +3,14 @@
 
 #define NULL 0
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef char int8_t;
-typedef short int16_t;
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef char            int8_t;
+typedef short           int16_t;
+typedef int             int32_t;
+typedef uint32_t        size_t;
+
+#define PACKED __attribute__((packed))
 
 #endif
