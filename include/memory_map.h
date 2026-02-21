eca5112cd2cadcefb839c140cc47120787fe02fe
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-02-21 16:10:03 +0100

    Swap STAGE1_STACK_BASE and STAGE1_STACK_TOP

--- include/memory_map.h
+++ include/memory_map.h
@@ -7,7 +7,9 @@
 #define STAGE1_OFFSET       0x8000
 #define STAGE1_SEGMENT      0x0000
 #define STAGE1_PHY_ADDR     (STAGE1_SEGMENT << 4) + STAGE1_OFFSET
-#define STAGE1_STACK_BASE   STAGE0_END      /* maybe it could be from STAGE0_PHY_ADDR */
-#define STAGE1_STACK_TOP    STAGE1_OFFSET
+#define STAGE1_STACK_BASE   STAGE1_OFFSET   /* i need to do something better */
+#define STAGE1_STACK_TOP    0x7000      /* I'm pulling these values out of my ass */
+
+#define FREE_MEM_ADDR       0x10000     /* literally out of my butt */
 
 #endif
