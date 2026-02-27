120144a1d5fb8151c411e3975fc9139ef0fcad1d
dcas796 <89193854+dcas796@users.noreply.github.com>
2026-02-27 11:38:48 +0100

    Change names of stack pointers

--- include/memory_map.h
+++ include/memory_map.h
@@ -2,14 +2,14 @@
 #define CONST_H
 
 #define STAGE0_PHY_ADDR     0x7c00
-#define STAGE0_END          STAGE0_PHY_ADDR + 512
+#define STAGE0_END          (STAGE0_PHY_ADDR + 512)
 
 #define STAGE1_OFFSET       0x8000
 #define STAGE1_SEGMENT      0x0000
-#define STAGE1_PHY_ADDR     (STAGE1_SEGMENT << 4) + STAGE1_OFFSET
-#define STAGE1_STACK_BASE   STAGE1_OFFSET   /* i need to do something better */
-#define STAGE1_STACK_TOP    0x7000      /* I'm pulling these values out of my ass */
+#define STAGE1_PHY_ADDR     ((STAGE1_SEGMENT << 4) + STAGE1_OFFSET)
+#define STAGE1_STACK_TOP    STAGE1_OFFSET   /* I need to do something better */
+#define STAGE1_STACK_BASE   0x7000      /* I'm pulling these values out of my ass */
 
-#define FREE_MEM_ADDR       0x10000     /* literally out of my butt */
+#define FREE_MEM_ADDR       0x80000     /* literally out of my butt */
 
 #endif
